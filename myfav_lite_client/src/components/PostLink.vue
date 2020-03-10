e1aeba0a4cd6d0e74c910e67fc6c6c74adf9a5f9
dimpurr <dimpurr@live.com>
2020-03-11 03:53:54 +0800

    try using weightless

---
+++ myfav_lite_client/src/components/PostLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <wl-card hoverable>
+    <wl-textarea outlined label="Post Link" style=" --textarea-max-height: 300px;"></wl-textarea>
+  </wl-card>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="stylus"></style>
